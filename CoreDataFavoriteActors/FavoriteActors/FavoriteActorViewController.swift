2d748ee1e42e907895d49ea8149a9b5b44d11c20
Jason Schatz <jason.schatz@udacity.com>
2015-07-10 14:24:30 -0700

    code for step-5.4

--- CoreDataFavoriteActors/FavoriteActors/FavoriteActorViewController.swift
+++ CoreDataFavoriteActors/FavoriteActors/FavoriteActorViewController.swift
@@ -10,15 +10,13 @@ import UIKit
 import CoreData
 
 /**
- * Challenge 1: Convert Favorite Actors to Fetched Results View Controller.
- */
+* Challenge 1: Convert Favorite Actors to Fetched Results View Controller.
+*/
 
-// Step 8: Add the NSFetchedResultsControllerDelegate protocol to the class declaration
-
-class FavoriteActorViewController : UITableViewController, ActorPickerViewControllerDelegate {
-   
-    // Step 4: Remove the actors array
-    var actors = [Person]()
+// Step 8: Add NSFetchedResultsControllerDelegate to class declaration
+class FavoriteActorViewController : UITableViewController, NSFetchedResultsControllerDelegate, ActorPickerViewControllerDelegate {
+    
+    // Step 3: Remove the actors array
     
     // MARK: - Life Cycle
     
@@ -27,12 +25,17 @@ class FavoriteActorViewController : UITableViewController, ActorPickerViewContro
         
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target: self, action: "addActor")
-
-        // This will be removed in step 5
-        actors = fetchAllActors()
+        
+        // This will be removed in step 4
+        // REMOVED
         
         // Step 2: invoke fetchedResultsController.performFetch(nil) here
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {}
+        
         // Step 9: set the fetchedResultsController.delegate = self
+        fetchedResultsController.delegate = self
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -47,23 +50,27 @@ class FavoriteActorViewController : UITableViewController, ActorPickerViewContro
         return CoreDataStackManager.sharedInstance().managedObjectContext
     }
     
-    // Step 1 - Add the lazy fetchedResultsController property. See the reference sheet.
-
-    // Step 5: Remove this method, and the invocation
+    // Step 1 - Add the lazy fetchedResultsController property. See the reference sheet in the lesson if you
+    // want additional help creating this property.
     
-    func fetchAllActors() -> [Person] {
+    lazy var fetchedResultsController: NSFetchedResultsController = {
         
-        // Create the Fetch Request
         let fetchRequest = NSFetchRequest(entityName: "Person")
         
-        // Execute the Fetch Request
-        do {
-            return try sharedContext.executeFetchRequest(fetchRequest) as! [Person]
-        } catch _ {
-            return [Person]()
-        }
-    }
-
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
+            managedObjectContext: self.sharedContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil)
+        
+        return fetchedResultsController
+        
+        }()
+    
+    
+    // Step 5: Remove the fetchAllActors() method, and the invocation
+    // REMOVED
     
     // Mark: - Actions
     
@@ -92,12 +99,12 @@ class FavoriteActorViewController : UITableViewController, ActorPickerViewContro
             ]
             
             // Now we create a new Person, using the shared Context
-            let actorToBeAdded = Person(dictionary: dictionary, context: sharedContext)
-
+            let _ = Person(dictionary: dictionary, context: sharedContext)
+            
             // Step 3: Do not add actors to the actors array.
             // This is no longer necessary once we are modifying our table through the
             // fetched results controller delefate methods
-            self.actors.append(actorToBeAdded)
+            // REMOVED
             
             CoreDataStackManager.sharedInstance().saveContext()
         }
@@ -110,53 +117,126 @@ class FavoriteActorViewController : UITableViewController, ActorPickerViewContro
     // This one is particularly tricky. You will need to get the "section" object for section 0, then
     // get the number of objects in this section. See the reference sheet for an example.
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return actors.count
+        let sectionInfo = self.fetchedResultsController.sections![section]
+        return sectionInfo.numberOfObjects
     }
     
+    
     // This one is easy. Get the actor using the following statement:
-    // 
+    //
     //        fetchedResultsController.objectAtIndexPath(:) as Person
     //
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let actor = actors[indexPath.row]
-        let CellIdentifier = "ActorCell"
-        
-        let cell = tableView.dequeueReusableCellWithIdentifier(CellIdentifier) as! ActorTableViewCell
-        
-        // This is new.
-        configureCell(cell, withActor: actor)
-        
-        return cell
+    override func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            let CellIdentifier = "ActorCell"
+            
+            // Here is how to replace the actors array using objectAtIndexPath
+            let actor = fetchedResultsController.objectAtIndexPath(indexPath) as! Person
+            
+            let cell = tableView.dequeueReusableCellWithIdentifier(CellIdentifier) as!
+            ActorTableViewCell
+            
+            // This is the new configureCell method
+            configureCell(cell, withActor: actor)
+            
+            return cell
     }
     
     // This one is also fairly easy. You can get the actor in the same way as cellForRowAtIndexPath above.
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let controller = storyboard!.instantiateViewControllerWithIdentifier("MovieListViewController") as! MovieListViewController
-        let actor = actors[indexPath.row]
-        
-        controller.actor = actor
-        
-        self.navigationController!.pushViewController(controller, animated: true)
+    override func tableView(tableView: UITableView,
+        didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            let controller =
+            storyboard!.instantiateViewControllerWithIdentifier("MovieListViewController")
+                as! MovieListViewController
+            
+            // Similar to the method above
+            let actor = fetchedResultsController.objectAtIndexPath(indexPath) as! Person
+            
+            controller.actor = actor
+            
+            self.navigationController!.pushViewController(controller, animated: true)
     }
     
     // This one is a little tricky. Instead of removing from the actors array you want to delete the actor from
-    // Core Data. 
-    // You can accomplish this in two steps. First get the actor object in the same way you did in the previous two methods. 
+    // Core Data.
+    // You can accomplish this in two steps. First get the actor object in the same way you did in the previous two methods.
     // Then delete the actor using this invocation
-    // 
+    //
     //        sharedContext.delete(actor)
     //
     // After that you do not need to delete the row from the table. That will be handled in the delegate. See reference sheet.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        
-        switch (editingStyle) {
-        case .Delete:
-            actors.removeAtIndex(indexPath.row)
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Fade)
-        default:
-            break
-        }
+    override func tableView(tableView: UITableView,
+        commitEditingStyle editingStyle: UITableViewCellEditingStyle,
+        forRowAtIndexPath indexPath: NSIndexPath) {
+            
+            switch (editingStyle) {
+            case .Delete:
+                
+                // Here we get the actor, then delete it from core data
+                let actor = fetchedResultsController.objectAtIndexPath(indexPath) as! Person
+                sharedContext.deleteObject(actor)
+                CoreDataStackManager.sharedInstance().saveContext()
+                
+            default:
+                break
+            }
     }
+    
+    
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        self.tableView.beginUpdates()
+    }
+    
+    func controller(controller: NSFetchedResultsController,
+        didChangeSection sectionInfo: NSFetchedResultsSectionInfo,
+        atIndex sectionIndex: Int,
+        forChangeType type: NSFetchedResultsChangeType) {
+            
+            switch type {
+            case .Insert:
+                self.tableView.insertSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+                
+            case .Delete:
+                self.tableView.deleteSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+                
+            default:
+                return
+            }
+    }
+    
+    //
+    // This is the most interesting method. Take particular note of way the that newIndexPath
+    // parameter gets unwrapped and put into an array literal: [newIndexPath!]
+    //
+    
+    func controller(controller: NSFetchedResultsController,
+        didChangeObject anObject: NSManagedObject,
+        atIndexPath indexPath: NSIndexPath?,
+        forChangeType type: NSFetchedResultsChangeType,
+        newIndexPath: NSIndexPath?) {
+            
+            switch type {
+            case .Insert:
+                tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+                
+            case .Delete:
+                tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+                
+            case .Update:
+                let cell = tableView.cellForRowAtIndexPath(indexPath!) as! ActorTableViewCell
+                let actor = controller.objectAtIndexPath(indexPath!) as! Person
+                self.configureCell(cell, withActor: actor)
+                
+            case .Move:
+                tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+                tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+            }
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        self.tableView.endUpdates()
+    }
+    
     
     // MARK: - Configure Cell
     
@@ -199,7 +279,6 @@ class FavoriteActorViewController : UITableViewController, ActorPickerViewContro
         }
     }
     
-    // Step 7: You can implmement the delegate methods here. Or maybe above the table methods. Anywhere is fine.
     
     // MARK: - Saving the array
     
