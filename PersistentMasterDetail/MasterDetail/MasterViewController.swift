4e3751065afd14aeb40f2b90f3a0324a4612b2cb
Ayush Saraswat <Ayush@dynamic2-233-002.usc.edu>
2015-11-11 16:32:04 -0800

    Added persistence after deleting + Updated MemoryMap for Swift 2.1

--- PersistentMasterDetail/MasterDetail/MasterViewController.swift
+++ PersistentMasterDetail/MasterDetail/MasterViewController.swift
@@ -82,8 +82,14 @@ class MasterViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
+            // Delete the object
             objects.removeAtIndex(indexPath.row)
+            
+            // Delete the row
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            
+            // Save the array
+            NSKeyedArchiver.archiveRootObject(objects, toFile: filePath)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
